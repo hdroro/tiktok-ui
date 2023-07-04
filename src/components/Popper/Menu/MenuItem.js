db9cd9bbba1379570052c423596f3f454733e4f2
hongdiem_roro <mingukmanse673@gmail.com>
2023-07-04 23:23:01 +0700

    use PropTypes in prj

--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Button from '~/components/Button';
 import classNames from 'classnames/bind';
 import styles from './Menu.module.scss';
@@ -9,15 +10,15 @@ function MenuItem({ data, onClick }) {
         separate: data.separate,
     });
     return (
-        <Button
-            className={classes}
-            leftIcon={data.icon}
-            to={data.to}
-            onClick={onClick}
-        >
+        <Button className={classes} leftIcon={data.icon} to={data.to} onClick={onClick}>
             {data.title}
         </Button>
     );
 }
+
+MenuItem.propTypes = {
+    data: PropTypes.object.isRequired,
+    onClick: PropTypes.func,
+};
 
 export default MenuItem;
